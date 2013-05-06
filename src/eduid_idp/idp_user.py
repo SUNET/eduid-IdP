81025d974540c97dd962336a399016e27c9eb418
Fredrik Thulin <fredrik@thulin.net>
2013-05-06 15:22:19 +0200

    import pysaml2/example/idp2 as a starting point

---
+++ src/eduid_idp/idp_user.py
@@ -0,0 +1,37 @@
+USERS = {
+    "roland": {
+        "surname": "Hedberg",
+        "givenName": "Roland",
+        "eduPersonScopedAffiliation": "staff@example.com",
+        "eduPersonPrincipalName": "rohe@example.com",
+        "uid": "rohe",
+        "eduPersonTargetedID": "one!for!all",
+        "c": "SE",
+        "o": "Example Co.",
+        "ou": "IT",
+        "initials": "P",
+        "schacHomeOrganization": "example.com",
+        "email": "roland@example.com",
+        "displayName": "P. Roland Hedberg",
+        "labeledURL": "http://www.example.com/rohe My homepage",
+        "norEduPersonNIN": "SE197001012222"
+    },
+    "babs": {
+        "surname": "Babs",
+        "givenName": "Ozzie",
+        "eduPersonAffiliation": "affiliate"
+    },
+    "upper": {
+        "surname": "Jeter",
+        "givenName": "Derek",
+        "eduPersonAffiliation": "affiliate"
+    },
+}
+
+EXTRA = {
+    "roland": {
+        "eduPersonEntitlement": "urn:mace:swamid.se:foo:bar",
+        "schacGender": "male",
+        "schacUserPresenceID": "skype:pepe.perez"
+    }
+}
