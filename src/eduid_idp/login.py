feeaa0b477d04fdfc6aa8202592fbb4708c66bda
Fredrik Thulin <fredrik@thulin.net>
2014-04-04 13:13:23 +0200

    Fix default entity categories data type.

--- src/eduid_idp/login.py
+++ src/eduid_idp/login.py
@@ -521,10 +521,10 @@ class SSO(Service):
         try:
             attributes = self.IDP.metadata.entity_attributes(ticket.req_info.message.issuer.text)
         except KeyError:
-            attributes = []
-        if "http://www.swamid.se/assurance-requirement" in attributes:
+            attributes = {}
+        if 'http://www.swamid.se/assurance-requirement' in attributes:
             # XXX don't just pick the first one from the list - choose the most applicable one somehow.
-            new_authn = attributes["http://www.swamid.se/assurance-requirement"][0]
+            new_authn = attributes['http://www.swamid.se/assurance-requirement'][0]
             requested = None
             if req_authn_context and req_authn_context.authn_context_class_ref:
                 requested = req_authn_context.authn_context_class_ref[0].text
