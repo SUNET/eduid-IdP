dbe579cee16ebee937e524bb3b28b6da20f42113
Fredrik Thulin <fredrik@thulin.net>
2014-04-24 13:26:52 +0200

    bump version to 0.3.14-dev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils import versionpredicate
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README')).read()
 
-version = '0.3.13'
+version = '0.3.14-dev'
 
 install_requires = [
     'pymongo == 2.6.3',
