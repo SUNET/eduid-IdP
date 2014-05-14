0c562a1560e8afedae0c841e93e696fcd506b5fb
Fredrik Thulin <fredrik@thulin.net>
2014-05-14 08:48:03 +0200

    version 0.3.15-dev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils import versionpredicate
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README')).read()
 
-version = '0.3.14'
+version = '0.3.15-dev'
 
 install_requires = [
     'pymongo == 2.6.3',
