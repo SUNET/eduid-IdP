8c25cdcd64ea51bcd9ca536607c6ed26a7ded893
Fredrik Thulin <fredrik@thulin.net>
2018-05-30 10:33:58 +0200

    make work with pymongo 3.6

--- src/eduid_idp/cache.py
+++ src/eduid_idp/cache.py
@@ -434,7 +434,7 @@ class SSOSessionCacheMDB(SSOSessionCache):
                 self.logger.error('Failed ensuring mongodb index, retrying ({})'.format(retry))
 
     def remove_session(self, sid):
-        res = self.sso_sessions.remove({'session_id': sid}, w = 1, getLastError = True)
+        res = self.sso_sessions.remove({'session_id': sid}, w = 1)
         try:
             return res['n']  # number of deleted records
         except (KeyError, TypeError):
