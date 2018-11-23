b86e97bbf8675a4898bc35edb885309bfcbe9460
Fredrik Thulin <fredrik@thulin.net>
2018-11-23 15:33:46 +0100

    better fix, from Ivan

--- src/eduid_idp/mischttp.py
+++ src/eduid_idp/mischttp.py
@@ -83,8 +83,8 @@ def create_html_response(binding, http_args, start_response, logger):
             status, pprint.pformat(http_args)))
 
     start_response(status, headers)
-    if six.PY2:
-        message = bytes(message)
+    if not isinstance(message, six.binary_type):
+        message = message.encode('utf-8')
     return message
 
 
