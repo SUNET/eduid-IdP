75f4335dd86594d6c5f8f99f932954b6422a505b
Fredrik Thulin <fredrik@thulin.net>
2019-02-05 16:07:38 +0100

    don't duplicate lists of requirements

--- setup.py
+++ setup.py
@@ -8,23 +8,9 @@ README = open(os.path.join(here, 'README')).read()
 
 version = '0.5.5'
 
-install_requires = [
-    'eduid_userdb >= 0.4.0',
-    'eduid_common[idp] >= 0.3.6b2',
-    'pysaml2 == 4.6.5',
-    'cherrypy == 15.0',
-    'defusedxml >= 0.5.0',
-    'six',
-]
-
-testing_extras = [
-    'nose == 1.3.7',
-    'coverage == 4.5.1',
-    'WebTest == 2.0.30',
-    'mock == 2.0.0',
-    'nosexcover >= 1.0.11',
-    'eduid_action>=0.2.1b2',
-]
+install_requires = [x for x in open(os.path.join(here, 'requirements.txt')).read().split('\n') if len(x) > 0]
+testing_extras = [x for x in open(os.path.join(here, 'test_requirements.txt')).read().split('\n')
+                  if len(x) > 0 and not x.startswith('-')]
 
 setup(name='eduid_idp',
       version=version,
@@ -41,8 +27,6 @@ setup(name='eduid_idp',
                 'eduid_idp.scripts',
                 ],
       package_dir = {'': 'src'},
-      #include_package_data=True,
-      #package_data = { },
       zip_safe=False,
       install_requires=install_requires,
       extras_require={
